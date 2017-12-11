e329f4f871a1ade9e6d5e6f1d76c8137362ac92a
Trevor Taubitz <trevor.taubitz@gmail.com>
2017-12-10 20:33:47 -0500

    Added templates and flask-admin

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from flask import Flask, session, request, render_template, g, redirect, url_for, abort
+from flask_security import Security, current_user
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from .models import db, user_datastore, User, Role
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Keep it secret, keep it safe'
@@ -8,30 +11,74 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 app.config['DEBUG'] = True
-db = SQLAlchemy(app)
-
-# Define models
-roles_users = db.Table('roles_users',
-                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+db.init_app(app)
 
 
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+def add_jinja_functions(app):
+    """Adds desired python functions to be accessible from Jinja templates"""
+    app.jinja_env.globals.update(
+        hasattr=hasattr,
+        enumerate=enumerate,
+        len=len
+    )
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+# Create customized model view class
+class SecurityModelView(ModelView):
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+
+        if current_user.has_role('superuser'):
+            return True
+
+        return False
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
 
 
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+def init_admin_extension(app):
+    admin = Admin(app, name='Web App Demo', template_mode='bootstrap3')
+    admin.add_view(SecurityModelView(User, db.session))
+    admin.add_view(SecurityModelView(Role, db.session))
+
+
+def init_security_extension_db(app):
+    security = Security(app, user_datastore)
+    user_datastore.create_role(name='superuser')
+    user_datastore.create_role(name='user')
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    g.session = session
+    g.path = request.path
+
+
+demo_pages = {
+
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=demo_pages)
+
+
+add_jinja_functions(app)
+init_admin_extension(app)
+init_security_extension_db(app)
+
+if __name__ == '__main__':
+    app.run()
--- manage.py
+++ manage.py
@@ -13,15 +13,21 @@ manager.add_command('runserver', Server())
 @manager.command
 def create_user():
     def create_and_add_user(email):
-        print('Adding new user')
-        password = prompt('Enter password: ')
-        admin_user = user_datastore.create_user(email=email, password=password)
-        db.session.add(admin_user)
+        password = prompt('Enter password')
+        user = user_datastore.create_user(email=email, password=password)
+        super_user = prompt_bool('Should user be admin?')
+        if super_user:
+            super_user_role = user_datastore.find_role('superuser')
+            user_datastore.add_role_to_user(user, super_user_role)
+        else:
+            user_role = user_datastore.find_role('user')
+            user_datastore.add_role_to_user(user, user_role)
+        db.session.add(user)
         db.session.commit()
-        print('New admin credentials are admin/{}'.format(password))
+        print('New credentials are admin/{}'.format(password))
 
     with app.app_context():
-        email = prompt('Enter email: ')
+        email = prompt('Enter email')
         user = user_datastore.get_user(email)
         if user:
             if prompt_bool('User already exists. Override?'):
